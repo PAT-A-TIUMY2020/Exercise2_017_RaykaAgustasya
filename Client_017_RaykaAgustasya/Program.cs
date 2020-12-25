2048545a1f54cc745c5d4967335fb8ddf164ca2e
Rayka Agustasya <raykaagustasya16@gmail.com>
2020-12-25 17:08:43 +0700

    Add project files.

---
+++ Client_017_RaykaAgustasya/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace Client_017_RaykaAgustasya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Server_017_RaykaAgustasya/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Service_017_RaykaAgustasya;
+using System.ServiceModel;
+
+
+namespace Server_017_RaykaAgustasya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostobjek = null;
+
+            try
+            {
+                hostobjek = new ServiceHost(typeof(TI_UMY));
+                hostobjek.Open();
+                Console.WriteLine("Server ready...");
+                Console.ReadLine();
+                hostobjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostobjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
